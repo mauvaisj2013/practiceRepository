81a4316fc3ea0ce1dd592f96e3e074651773883e
mauvaisj2013 <99113309+mauvaisj2013@users.noreply.github.com>
2022-03-30 19:34:02 -0400

    Last minute HW stuff

---
+++ Homework Travel Plans/script.js
@@ -0,0 +1,27 @@
+import myTravelPlans from "./mytravelplans.js";
+
+const markup = (myTravelPlans) => {
+    return `
+    <div>
+        <h3>${myTravelPlans.name}</h3>
+        <ul>
+        <li>Destination:${myTravelPlans.location}</li>
+        <li>Number of Days:${myTravelPlans.numberOfDays}</li>
+        <li>Why am I going?:${myTravelPlans.reason}</li>
+        <li>Who's coming with me?:${myTravelPlans.companion}</li>
+        <li>Year of the trip?:${myTravelPlans.dateOfDeparture.year}</li>
+        <li>Month of the departure?:${myTravelPlans.dateOfDeparture.month}</li>
+        <li>Day that I'm leaving?:${myTravelPlans.dateOfDeparture.day}</li>
+        <li>Will I get laid?:${myTravelPlans.lucky ? "Hopefully!" : "Hell naw"}
+        <li>Things I HAVE to do:${myTravelPlans.activitiesPlanned.first}</li>
+        <li>Places that I want to visit:${myTravelPlans.activitiesPlanned.forSightseeing}</li>
+        <li>The goal is to not speak English:${myTravelPlans.activitiesPlanned.languagePractice}</li>
+        <li>Who I'm meeting up with:${myTravelPlans.activitiesPlanned.family}</li>
+        <li>Anything I'm forgetting? Oh, yeah!:${myTravelPlans.activitiesPlanned.lastly}</li>
+        </ul>
+    <div>     `
+}
+
+const main = document.createElement("main");
+main.innerHTML = markup(myTravelPlans);
+document.body.appendChild(main);
